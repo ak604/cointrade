7c605d7709a9d011577768cfccc405ccef723e57
anand kumar <anand.kumar@rockyou.com>
2017-12-13 11:09:10 +0530

    second commit

--- cointrade/app/score/Calc.scala
+++ cointrade/app/score/Calc.scala
@@ -9,25 +9,28 @@ import play.api.Logger
 @Singleton
 class Calc @Inject()(val coinPrice : CoinPrice, val ec: ExecutionContext) {
   
+  
+  val weightByHour = Array(1,2,3,8,10,9,8,7,6,5,4,3,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1)
   val interval=5;
   def topN(n :Int)={
     
     val scoreMap: List[(String,Int)] = Nil
-    Coins.lst.take(3).foreach{coin=>
+    Coins.lst.take(1).foreach{coin=>
       scoreMap++List(coin, score(coin))
     }
     scoreMap
   }
+   
   
- 
   def score(coin:String):Int={
       
      implicit val localec : ExecutionContext= ec
     
      val div:Int=60/interval;
       val values = coinPrice.lastNHours(coin, 12, interval);
-      val normalized = values.map{lst =>
-         lst.zipWithIndex
+      val normalizedx = values.map{lst =>
+        
+        val normalized=lst.zipWithIndex
         .filter { case(value,i) => ((i+div)%div <5)  }
         .map{ case(e,_)=> e}
         .grouped(5)
@@ -35,10 +38,17 @@ class Calc @Inject()(val coinPrice : CoinPrice, val ec: ExecutionContext) {
           (lst5.sum/5)
         }.toList
         
+        val diff =normalized.drop(1).zip(normalized.dropRight(1)).map{
+          case(x,y) =>  Logger.debug(x +" "+y);((1000*(y-x))/x)
+        }
+        
+        
       }
-      normalized.foreach{f =>
+      
+      
+      normalizedx.foreach{f =>
        Logger.debug( f.mkString("-"))
       }
-      5      
+     5
   }
 }
